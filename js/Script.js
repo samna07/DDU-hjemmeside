0674ae48b6b75b404d15c1d6893b2ba34eb91a27
Theogmaria <Theoogmaria1@gmail.com>
2026-04-22 08:27:36 +0200

    Home menu added
    Plus Kort første version og Lær men uden video

---
+++ js/Script.js
@@ -0,0 +1,9 @@
+
+
+function openPopup() {
+  document.getElementById("popup").classList.add("active");
+}
+
+function closePopup() {
+  document.getElementById("popup").classList.remove("active");
+}
